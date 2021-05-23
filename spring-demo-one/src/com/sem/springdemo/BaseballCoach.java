1af27572bea5e509b1b7f85915b56af76c23b916
semihlevent <semihlt@hotmail.com>
2021-05-24 01:13:12 +0300

    comment deleted

--- spring-demo-one/src/com/sem/springdemo/BaseballCoach.java
+++ spring-demo-one/src/com/sem/springdemo/BaseballCoach.java
@@ -1,7 +1,7 @@
 package com.sem.springdemo;
 
 public class BaseballCoach implements Coach {
-    // comment
+    //
     @Override
     public String getDailyWorkout() {
         return "Spend 30 minutes on batting practice";
