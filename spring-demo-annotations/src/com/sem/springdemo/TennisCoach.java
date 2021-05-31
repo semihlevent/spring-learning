6bea5d6e680175795de3e893960b99a1db212ad5
semihlevent <semihlt@hotmail.com>
2021-06-01 01:04:25 +0300

    different injection types with annotations

--- spring-demo-annotations/src/com/sem/springdemo/TennisCoach.java
+++ spring-demo-annotations/src/com/sem/springdemo/TennisCoach.java
@@ -1,11 +1,41 @@
 package com.sem.springdemo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class TennisCoach implements Coach {
+
+    private FortuneService fortuneService;
+
+    public TennisCoach() {
+        System.out.println(">> TennisCoach: inside default constructor");
+    }
+
+//    @Autowired //Setter Injection
+//    public void setFortuneService(FortuneService fortuneService) {
+//        System.out.println(">> TennisCoach: inside setFortuneService method");
+//        this.fortuneService = fortuneService;
+//    }
+
+    @Autowired //Method Injection
+    public void doSomeCrazyStuff(FortuneService fortuneService) {
+        System.out.println(">> TennisCoach: inside setFortuneService method");
+        this.fortuneService = fortuneService;
+    }
+
+//    @Autowired // Constructor Injection
+//    public TennisCoach(FortuneService fortuneService) {
+//        this.fortuneService = fortuneService;
+//    }
+
     @Override
     public String getDailyWorkout() {
         return "Practice your backhand volley";
+    }
+
+    @Override
+    public String getDailyFortune() {
+        return fortuneService.getFortune();
     }
 }
