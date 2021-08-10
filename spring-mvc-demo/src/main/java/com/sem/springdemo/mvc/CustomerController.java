988b2d29eb4f04c85c5153defe00b7d86822b6ef
semihlevent <semihlt@hotmail.com>
2021-08-11 01:10:01 +0300

    new fields and validataions added

--- spring-mvc-demo/src/main/java/com/sem/springdemo/mvc/CustomerController.java
+++ spring-mvc-demo/src/main/java/com/sem/springdemo/mvc/CustomerController.java
@@ -2,15 +2,24 @@ package com.sem.springdemo.mvc;
 
 import javax.validation.Valid;
 
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/customer")
 public class CustomerController {
+	
+	@InitBinder
+	public void initBinder(WebDataBinder webDataBinder) {
+		StringTrimmerEditor stringTrimmerEditor = new StringTrimmerEditor(true);
+		webDataBinder.registerCustomEditor(String.class, stringTrimmerEditor);
+	}
 	
 	@RequestMapping("/showForm")
 	public String showForm(Model model) {
@@ -23,8 +32,8 @@ public class CustomerController {
 	public String processForm(
 			@Valid @ModelAttribute("customer") Customer customer,
 			BindingResult bindingResult) {
-		System.out.println("|".concat(customer.getFirstName()).concat("|"));
-		System.out.println("|".concat(customer.getLastName()).concat("|"));
+		System.out.println("|" + customer.getFirstName() +"|");
+		System.out.println("|" + customer.getLastName() + "|");
 		if (bindingResult.hasErrors()) {
 			return "customer-form";
 		}
--- spring-mvc-demo/src/main/webapp/WEB-INF/view/customer-confirmation.jsp
+++ spring-mvc-demo/src/main/webapp/WEB-INF/view/customer-confirmation.jsp
@@ -6,7 +6,11 @@
 	</head>
 	<body>
 		The customer is confirmed: ${customer.firstName} ${customer.lastName}
-	
+		<br><br>
+		Free passes: ${customer.freePasses}
+		<br><br>
+		Postal code: ${customer.postalCode}
+		<br><br>
+		Course code: ${customer.courseCode}
 	</body>
-
 </html>
